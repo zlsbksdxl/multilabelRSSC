47f28d3d44415b0edf6aefe0d7dd87734da04cc4
TangXu <74549002+TangXu-Group@users.noreply.github.com>
2025-04-09 15:49:54 +0800

    Update model_auxiliary.py

---
+++ MLMamba/model_auxiliary.py
@@ -0,0 +1,56 @@
+import torch.nn as nn
+import torchvision
+import torch
+import math
+class FBackbone(nn.Module):
+
+    def __init__(self, net):
+        super().__init__()
+
+        if net == 'resnet18':
+            self.backbone = torchvision.models.resnet18(pretrained=True)
+        elif net == 'resnet50':
+            self.backbone = torchvision.models.resnet50(pretrained=True)
+        else:
+            raise ValueError('模型错误')
+
+    def forward(self, x):
+        x = self.backbone.conv1(x)
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+        return x
+    
+class GroupWiseLinear(nn.Module):
+    # could be changed to: 
+    # output = torch.einsum('ijk,zjk->ij', x, self.W)
+    # or output = torch.einsum('ijk,jk->ij', x, self.W[0])
+    def __init__(self, num_class, hidden_dim, bias=True):
+        super().__init__()
+        self.num_class = num_class
+        self.hidden_dim = hidden_dim
+        self.bias = bias
+
+        self.W = nn.Parameter(torch.Tensor(1, num_class, hidden_dim))
+        if bias:
+            self.b = nn.Parameter(torch.Tensor(1, num_class))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.W.size(2))
+        for i in range(self.num_class):
+            self.W[0][i].data.uniform_(-stdv, stdv)
+        if self.bias:
+            for i in range(self.num_class):
+                self.b[0][i].data.uniform_(-stdv, stdv)
+
+    def forward(self, x):
+        # x: B,K,d
+        x = (self.W * x).sum(-1)
+        if self.bias:
+            x = x + self.b
+        return x
